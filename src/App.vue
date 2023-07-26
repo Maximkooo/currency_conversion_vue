63bea4c3abb26721f88495f2b1080a5928a9461e
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2023-07-26 18:54:10 +0200

    main code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,134 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container w-50 pt-5 pb-5">
+    <b-form @submit.prevent="onSubmit">
+      <b-form-group label="Amount">
+        <b-form-input
+          v-model="form.amount"
+          type="number"
+          placeholder="Enter amount"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="From currency" class="mt-3">
+        <b-form-select
+          v-model="form.from_currency"
+          :options="fromCurrencyOptions"
+          :plain="true"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group label="To currency" class="mt-3">
+        <b-form-select
+          v-model="form.to_currency"
+          :options="toCurrencyOptions"
+          :plain="true"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <div class="row mt-4">
+        <div class="col-6">
+          <b-button type="submit" variant="primary">Convert</b-button>
+        </div>
+        <div class="col-6">
+          <h3>Value: {{ currentValue }}</h3>
+        </div>
+      </div>
+    </b-form>
+
+    <div v-if="history.length">
+      <h1 class="text-center mt-5">History</h1>
+      <History :items="history" @deleteItem="deleteItem" />
+      <b-button
+        variant="danger"
+        class="text-center mx-auto d-block"
+        @click="() => (history = [])"
+        >Clear history</b-button
+      >
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import History from "./components/History.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { History },
+  data() {
+    return {
+      currentValue: 0,
+      currencies: [],
+      history: [],
+      form: {},
+      fromCurrencyOptions: [
+        { text: "USD", value: { name: "USD", code: 840 } },
+        { text: "EUR", value: { name: "EUR", code: 978 } },
+        { text: "CHF", value: { name: "CHF", code: 756 } },
+        { text: "GBP", value: { name: "GBP", code: 826 } },
+        { text: "JOD", value: { name: "JOD", code: 400 } },
+        { text: "KWD", value: { name: "KWD", code: 414 } },
+        { text: "KZT", value: { name: "KZT", code: 398 } },
+        { text: "LBP", value: { name: "LBP", code: 422 } },
+        { text: "MAD", value: { name: "MAD", code: 504 } },
+      ],
+      toCurrencyOptions: [{ text: "UAH", value: { name: "UAH", code: 980 } }],
+    };
+  },
+  created() {
+    this.getData();
+
+    this.interval = setInterval(() => {
+      this.getData();
+    }, 1000 * 60);
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    onSubmit() {
+      const { amount, from_currency, to_currency } = this.form;
+
+      const currency =
+        this.currencies.find(
+          (item) =>
+            item.currencyCodeA === from_currency.code &&
+            item.currencyCodeB === to_currency.code
+        ) || {};
+
+      if (Object.keys(currency).length) {
+        this.limitHistory();
+        this.currentValue = (
+          amount * (currency.rateSell || currency.rateCross)
+        ).toFixed(2);
+
+        this.history.push({
+          id: Math.floor(Math.random() * 10000),
+          amount,
+          from_currency,
+          to_currency,
+          actions: this.currentValue,
+        });
+      }
+    },
+    limitHistory() {
+      if (this.history.length >= 20) {
+        this.history = this.history.splice(1, this.history.length - 1);
+      }
+    },
+    deleteItem(item) {
+      this.history = this.history.filter((el) => item.id !== el.id);
+    },
+    getData() {
+      axios
+        .get(`https://api.monobank.ua/bank/currency`)
+        .then((res) => (this.currencies = res.data));
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/History.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-table :fields="fields" :items="items">
+    <template v-slot:cell(from_currency)="{ item }">
+      <span class="text-truncate">{{ item.from_currency.name }}</span>
+    </template>
+    <template v-slot:cell(to_currency)="{ item }">
+      <span class="text-truncate">{{ item.to_currency.name }}</span>
+    </template>
+    <template v-slot:cell(delete)="{ item }">
+      <b-button size="sm" variant="danger" @click="removeItem(item)"
+        >Delete
+      </b-button>
+    </template>
+  </b-table>
+</template>
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: "amount",
+          label: "Amount",
+          class: "align-middle",
+        },
+        {
+          key: "from_currency",
+          label: "From currency",
+          class: "align-middle",
+        },
+        {
+          key: "to_currency",
+          label: "To currency",
+          class: "align-middle",
+        },
+        {
+          key: "actions",
+          label: "Actions",
+          class: "align-middle",
+        },
+        {
+          key: "delete",
+          label: "Effect",
+        },
+      ],
+    };
+  },
+  methods: {
+    removeItem(item) {
+      this.$emit("deleteItem", item);
+    },
+  },
+};
+</script>
