133c4508a8a41c3bd02647022664194c621f42ac
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2023-07-26 19:43:24 +0200

    styles

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import('../styles/styles.css')
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
